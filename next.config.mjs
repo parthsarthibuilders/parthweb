7477c77df5d06eb810bd26cb4f2e7d23b13ba1fc
teamparmjeet <teamparmjeet@gmail.com>
2025-05-03 12:42:04 +0530

    new

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-      domains: ['parthsarthibuilder.com','parthsarthi.s3.eu-north-1.amazonaws.com','www.parthsarthi.org', 'res.cloudinary.com','encrypted-tbn0.gstatic.com','d199amilqw5tyr.cloudfront.net','download.logo.wine','mir-s3-cdn-cf.behance.net','www.thereportingtoday.com','parthweb.vercel.app'], // Added Cloudinary domain here
+      domains: ['parthsarthibuilder.com','parthsarthi98761.s3.eu-north-1.amazonaws.com','www.parthsarthi.org', 'res.cloudinary.com','encrypted-tbn0.gstatic.com','d199amilqw5tyr.cloudfront.net','download.logo.wine','mir-s3-cdn-cf.behance.net','www.thereportingtoday.com','parthweb.vercel.app'], // Added Cloudinary domain here
   },
 };
 
