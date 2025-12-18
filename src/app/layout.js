dcbfef7a9d14ba0eea9215415ad065af47314c5b
Rahul <147478589+iitian12@users.noreply.github.com>
2025-12-19 04:53:38 +0530

    googleTagManager

--- src/app/layout.js
+++ src/app/layout.js
@@ -59,6 +59,21 @@ export default function RootLayout({ children }) {
 
   return (
     <html lang="en">
+      <head>
+        <script
+          id="gtm-script"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','GTM-54B2BSRQ');
+            `,
+          }}
+        />
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
@@ -72,6 +87,9 @@ export default function RootLayout({ children }) {
             __html: JSON.stringify(schemaData),
           }}
         />
+
+        <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-54B2BSRQ"
+          height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
 
         {/* <!-- Google tag (gtag.js) --> */}
         <script
